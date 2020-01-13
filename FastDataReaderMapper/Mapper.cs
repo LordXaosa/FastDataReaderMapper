a432541fa42e8d8f8414b2dc4b1e5f491e4a2f57
LordXaosa <nikitozich@mail.ru>
2020-01-13 10:42:34 +0300

    Code cleanup and faster property setter for netstandard 2.1

--- FastDataReaderMapper/Mapper.cs
+++ FastDataReaderMapper/Mapper.cs
@@ -4,6 +4,7 @@ using System.ComponentModel.DataAnnotations.Schema;
 using System.Data;
 using System.Linq.Expressions;
 using System.Reflection;
+using System.Reflection.Emit;
 using System.Runtime.CompilerServices;
 using System.Threading;
 using System.Threading.Tasks;
@@ -15,61 +16,96 @@ namespace FastDataReaderMapper
         public static List<T> Map<T>(this IDataReader reader) where T : new()
         {
             List<T> list = new List<T>();
-            HashSet<string> tableFields = new HashSet<string>();
-            for (int i = 0; i < reader.FieldCount; i++)
-                tableFields.Add(reader.GetName(i).ToLower());
-
-            PropertyInfo[] properties = typeof(T).GetProperties();
-            Dictionary<PropertyInfo, string> nameDict = new Dictionary<PropertyInfo, string>();
-            Dictionary<PropertyInfo, Type> propertyType = new Dictionary<PropertyInfo, Type>();
-            Dictionary<PropertyInfo, Action<T, object>> setters = new Dictionary<PropertyInfo, Action<T, object>>();
-            Dictionary<string, string> lowerCaseNames = new Dictionary<string, string>();
-            foreach (PropertyInfo pi in properties)
-            {
-                string name = pi.GetCustomAttribute<ColumnAttribute>()?.Name ?? pi.Name;
-                nameDict.Add(pi, name);
-                lowerCaseNames.Add(name, name.ToLower());
-                propertyType.Add(pi, Nullable.GetUnderlyingType(pi.PropertyType) ?? pi.PropertyType);
-                setters.Add(pi, BuildUntypedSetter<T>(pi));
-            }
+            PrepareType(reader, out PropertyInfo[] properties, out HashSet<string> tableFields,
+                out Dictionary<PropertyInfo, (string Name, Type PropertyType, Action<T, object> Setter, string LowerCasename)> header);
             while (reader.Read())
             {
-                T result = new T();
-                foreach (PropertyInfo pi in properties)
-                {
-                    string name = nameDict[pi];
-
-                    if (!string.IsNullOrEmpty(name) && tableFields.Contains(lowerCaseNames[name]))
-                    {
-                        object val = reader[name];
-                        Type dataType = val.GetType();
-                        if (pi.PropertyType.IsAssignableFrom(dataType))
-                            setters[pi](result, val);
-                        else if (dataType == typeof(DBNull))
-                            setters[pi](result, null);
-                        else
-                        {
-                            object safeValue = (val == null) ? null : Convert.ChangeType(val, propertyType[pi]);
-                            setters[pi](result, safeValue);
-                        }
-                    }
-                }
-                list.Add(result);
+                list.Add(MapRow(reader, properties, tableFields, header));
             }
             return list;
         }
-
-        private static Action<T, object> BuildUntypedSetter<T>(PropertyInfo propertyInfo)
+        public static async IAsyncEnumerable<T> MapAsync<T>(this IDataReader reader, [EnumeratorCancellation]CancellationToken cancellationToken = default) where T : new()
         {
-            var targetType = propertyInfo.DeclaringType;
-            var methodInfo = propertyInfo.GetSetMethod();
+            PrepareType(reader, out PropertyInfo[] properties, out HashSet<string> tableFields,
+                out Dictionary<PropertyInfo, (string Name, Type PropertyType, Action<T, object> Setter, string LowerCasename)> header);
+            while (reader.Read())
+            {
+                if (cancellationToken.IsCancellationRequested)
+                    throw new OperationCanceledException(cancellationToken);
+                yield return MapRow(reader, properties, tableFields, header);
+            }
+        }
+
+        private static void PrepareType<T>(IDataReader reader, out PropertyInfo[] properties, out HashSet<string> tableFields,
+            out Dictionary<PropertyInfo, (string Name, Type PropertyType, Action<T, object> Setter, string LowerCasename)> header) where T: new()
+        {
+            tableFields = new HashSet<string>();
+            for (int i = 0; i < reader.FieldCount; i++)
+                tableFields.Add(reader.GetName(i).ToLower());
+
+            properties = typeof(T).GetProperties();
+            header = new Dictionary<PropertyInfo, (string Name, Type PropertyType, Action<T, object> Setter, string LowerCasename)>();
+            foreach (PropertyInfo pi in properties)
+            {
+                string name = pi.GetCustomAttribute<ColumnAttribute>()?.Name ?? pi.Name;
+                header.Add(pi, (name, Nullable.GetUnderlyingType(pi.PropertyType) ?? pi.PropertyType, BuildUntypedSetter<T>(pi), name.ToLower()));
+            }
+        }
+
+        private static T MapRow<T>(IDataRecord row, PropertyInfo[] properties, HashSet<string> tableFields,
+            Dictionary<PropertyInfo, (string Name, Type PropertyType, Action<T, object> Setter, string LowerCasename)> header) where T: new()
+        {
+            T result = new T();
+            foreach (PropertyInfo pi in properties)
+            {
+                (string Name, Type PropertyType, Action<T, object> Setter, string LowerCasename) tuple = header[pi];
+                string name = tuple.Name;
+
+                if (!string.IsNullOrEmpty(name) && tableFields.Contains(tuple.LowerCasename))
+                {
+                    object val = row[name];
+                    Type dataType = val.GetType();
+                    if (pi.PropertyType.IsAssignableFrom(dataType))
+                        tuple.Setter(result, val);
+                    else if (dataType == typeof(DBNull))
+                        tuple.Setter(result, null);
+                    else
+                    {
+                        object safeValue = (val == null) ? null : Convert.ChangeType(val, tuple.PropertyType);
+                        tuple.Setter(result, safeValue);
+                    }
+                }
+            }
+            return result;
+        }
+        private static Action<T, object> BuildUntypedSetter<T>(PropertyInfo pi) where T: new()
+        {
+#if NETSTANDARD2_0
+            var targetType = pi.DeclaringType;
+            var methodInfo = pi.GetSetMethod();
             var exTarget = Expression.Parameter(targetType, "t");
             var exValue = Expression.Parameter(typeof(object), "p");
             var exBody = Expression.Call(exTarget, methodInfo,
-               Expression.Convert(exValue, propertyInfo.PropertyType));
+               Expression.Convert(exValue, pi.PropertyType));
             var lambda = Expression.Lambda<Action<T, object>>(exBody, exTarget, exValue);
             var action = lambda.Compile();
             return action;
+#elif NET461 || NETSTANDARD2_1 || NETCOREAPP3_0 || NETCOREAPP3_1
+            DynamicMethod method = new DynamicMethod(
+                "PropertySetter",
+                typeof(void),
+                new[] { typeof(T), typeof(object) },
+                Assembly.GetExecutingAssembly().ManifestModule);
+            ILGenerator il = method.GetILGenerator(100);
+            il.Emit(OpCodes.Ldarg_0);
+            il.Emit(OpCodes.Ldarg_1);
+            //if (pi.PropertyType.IsValueType)
+            if (pi.PropertyType == typeof(DateTime))
+                il.Emit(OpCodes.Unbox_Any, pi.PropertyType);
+            il.EmitCall(OpCodes.Callvirt, pi.GetSetMethod(), null);
+            il.Emit(OpCodes.Ret);
+            return (Action<T, object>)method.CreateDelegate(typeof(Action<T, object>));
+#endif
         }
 
         private static Func<T, object> BuildUntypedGetter<T>(PropertyInfo propertyInfo)
@@ -86,51 +122,6 @@ namespace FastDataReaderMapper
 
             var action = lambda.Compile();
             return action;
-        }
-
-        public static async IAsyncEnumerable<T> MapAsync<T>(this IDataReader reader, [EnumeratorCancellation]CancellationToken cancellationToken = default) where T : new()
-        {
-            HashSet<string> tableFields = new HashSet<string>();
-            for (int i = 0; i < reader.FieldCount; i++)
-                tableFields.Add(reader.GetName(i).ToLower());
-
-            PropertyInfo[] properties = typeof(T).GetProperties();
-            Dictionary<PropertyInfo, string> nameDict = new Dictionary<PropertyInfo, string>();
-            Dictionary<PropertyInfo, Type> propertyType = new Dictionary<PropertyInfo, Type>();
-            Dictionary<PropertyInfo, Action<T, object>> setters = new Dictionary<PropertyInfo, Action<T, object>>();
-            Dictionary<string, string> lowerCaseNames = new Dictionary<string, string>();
-            foreach (PropertyInfo pi in properties)
-            {
-                string name = pi.GetCustomAttribute<ColumnAttribute>()?.Name ?? pi.Name;
-                nameDict.Add(pi, name);
-                lowerCaseNames.Add(name, name.ToLower());
-                propertyType.Add(pi, Nullable.GetUnderlyingType(pi.PropertyType) ?? pi.PropertyType);
-                setters.Add(pi, BuildUntypedSetter<T>(pi));
-            }
-            while (reader.Read())
-            {
-                T result = new T();
-                foreach (PropertyInfo pi in properties)
-                {
-                    string name = nameDict[pi];
-
-                    if (!string.IsNullOrEmpty(name) && tableFields.Contains(lowerCaseNames[name]))
-                    {
-                        object val = reader[name];
-                        Type dataType = val.GetType();
-                        if (pi.PropertyType.IsAssignableFrom(dataType))
-                            setters[pi](result, val);
-                        else if (dataType == typeof(DBNull))
-                            setters[pi](result, null);
-                        else
-                        {
-                            object safeValue = (val == null) ? null : Convert.ChangeType(val, propertyType[pi]);
-                            setters[pi](result, safeValue);
-                        }
-                    }
-                }
-                yield return result;
-            }
         }
     }
 }
